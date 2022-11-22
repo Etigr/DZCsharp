39f2b7a6632f4b7ac9dffe95f5e5a7714cf2cf82
Evgen <fadi_86@mail.ru>
2022-11-22 23:52:19 +0300

    2 задачи дз5

--- DZ5/Program.cs
+++ DZ5/Program.cs
@@ -1,8 +1,83 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
+/*
+int [] RandomThree (int size, int minVal, int maxVal)
+{
+    int [] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minVal, maxVal + 1);
+    } 
+    return array;
+}
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+int ChetSum(int [] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            sum += 1;
+        }
+    }
+    return sum;
+}
+Console.Write("Введите размер массива: ");
+int usersize = Convert.ToInt32(Console.ReadLine());
+int min = 100;
+int max = 999;
 
+int [] newArray = RandomThree(usersize, min, max);
+PrintArray(newArray);
+
+Console.Write($"Количествово чётных чисел в массиве: {ChetSum(newArray)}");
+*/
 
 // Задача 36: Задайте массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int [] RandomThree (int size, int minVal, int maxVal)
+{
+    int [] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minVal, maxVal + 1);
+    } 
+    return array;
+}
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+int ChetSum(int [] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+        sum += array[i];  
+    return sum;
+}
+Console.Write("Введите размер массива: ");
+int usersize = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение минимального значения массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение максимального значения массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int [] newArray = RandomThree(usersize, min, max);
+PrintArray(newArray);
+
+Console.Write($"Сумма элементов на нечётных позициях равна: {ChetSum(newArray)}");
 
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
